09c890bddaf868f09aaa7df6c02cbd2681e45d0b
Żegota Marta <marta.zegota@pgn.corp>
2022-04-23 10:10:54 +0200

    Add liczby.py

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("hello")
+shopping_dict = {
+    "piekarnia": ['chleb', 'pączek', 'bułki'],
+    "warzywniak": ['marchew', 'seler', 'rukola']
+}
+
+shops = shopping_dict.keys()
+products = shopping_dict.values()
+product = []
+shop = []
+
+shop_dict = dict(shopping_dict)
+for shop, product in shop_dict.items():
+  print(f"Idę do {shop} i kupuję następujące {product}")
+  for products in product:
+      print(products.capitalize())
